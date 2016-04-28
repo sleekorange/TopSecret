85b4924b75532ee8199f778a549d8aa927d06e92
Hans Mygind <hmygind@gmail.com>
2016-04-28 13:05:36 +0200

    Login Version 0.001

--- Hans/register.php
+++ Hans/register.php
@@ -1,5 +1,22 @@
 <?php
+if(isset($_POST['username'])){
+$username = $_POST['username'];
+echo "Something works!".$username;
+}
 
-echo "Something works!";
 
-?> 
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+  	<form method="POST">
+  		<input type="text" placeholder="username" name="username"></input>
+  		<input type="password" placeholder="password" name="password">
+		<button type="submit">Submit</button>
+  	</form>
+  </body>
+</html> 
