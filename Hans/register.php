5fcd8d7312258101c94f0b85651c7400a8df25ed
Hans Mygind <hmygind@gmail.com>
2016-04-28 14:59:02 +0200

    Adding basic functions Salt + Hash

--- Hans/register.php
+++ Hans/register.php
@@ -1,7 +1,41 @@
 <?php
-if(isset($_POST['username'])){
-$username = $_POST['username'];
-echo "Something works!".$username;
+// Methods:
+function saltFunction ( $password ) {
+			$salt = mcrypt_create_iv(50, MCRYPT_DEV_URANDOM);
+			$passwordSalt = $password . $salt;
+			$passwordArray = array('passwordSalt' => $passwordSalt, 'salt' => $salt);
+			return $passwordArray;
+		}
+
+function hashFunction ( $password ) {
+			$passwordHash = password_hash($password, PASSWORD_DEFAULT)."\n";
+			return $passwordHash;
+		}
+
+
+if(isset($_POST['username']) && isset($_POST['password'])){
+	if(!empty($_POST['username']) && !empty($_POST['password'])){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		echo "Something works!".$username.$password;
+		echo "<br>";
+
+		
+
+		$passwordSalted = saltFunction("string")['passwordSalt'];
+
+
+		
+
+		//$salt = mcrypt_create_iv(50, MCRYPT_DEV_URANDOM);
+		//$passwordSalt = $password . $salt;
+		// $passwordHash = password_hash($passwordSalt, PASSWORD_DEFAULT)."\n";
+		// $hashedSalt = hash('sha512',$salt);
+		var_dump($passwordSalted);
+		var_dump(hashFunction($passwordSalted));
+	}
+	
+
 }
 
 
