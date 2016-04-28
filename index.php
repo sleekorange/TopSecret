884bb92fc491f85cc4f9bdcbfaaa11e67a85f009
Johan Utzon <jrubech@gmail.com>
2016-04-28 15:08:37 +0200

    Added core/init.php + Several Design features and folders

--- index.php
+++ index.php
@@ -1,4 +1,66 @@
 <?php
-	echo "Top Secret Project!!";
-	echo "these are secrets!";
+	require_once 'core/init.php';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rate my pet</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,600,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div id="wrapper">
+	<div id="top">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<h3>Rate my pet</h3>
+				</div>
+				<div class="col-sm-6">
+					<div class="loginHolder">
+						<button class="loginBtn">Login <i class="fa fa-lock"></i></button>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="banner">
+
+	</div>
+
+	<div id="content">
+		<div class="container">
+			<div class="frontText">
+				<h1>Rate my pet</h1>
+				<p>Lorem ipsum dolor sit amet, cursus volutpat volutpat tempor urna bibendum, ultrices arcu in. </p>
+				<p>Lorem ipsum dolor sit amet, cursus volutpat volutpat tempor urna bibendum. </p>
+			</div>
+			
+
+			</div>
+		</div>
+	</div>
+
+	<div id="footer">
+		<div class="container">
+		Rate my Pet - Copyright 2016 ©
+		</div>
+	</div>
+
+</div>
+	
+	
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+
+
+</body>
+</html>
