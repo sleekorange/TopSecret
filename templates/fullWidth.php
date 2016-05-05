0416f0b3cf7423512c3de8c243fb34472b958632
Jonas Poulsen <jonasppoulsen@gmail.com>
2016-05-05 11:34:24 +0200

    ready to merge with Hans

--- templates/fullWidth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-?>
-
-<div class="row">
-	<div class="col-sm-12">
-		<?php echo $content ?>
-	</div>
-</div>
--- templates/split.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-	<div class="col-sm-6">
-		<?php echo $content ?>
-	</div>
-	<div class="col-sm-6">
-		<?php echo $contentTwo ?>
-	</div>
-</div>
